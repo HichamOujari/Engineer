602b0d2c25ad712cdf6ca4c5f79587190fd3e573
Hicham Oujari <hichamoujari99@gmail.com>
2021-03-24 09:29:31 +0100

    sls

--- component/Project/textCardDisplayer.js
+++ component/Project/textCardDisplayer.js
@@ -8,14 +8,27 @@ export default class TextCardDisplayer extends Component {
   componentDidMount(){
     AOS.init({duration:2000});
   }
+  state={
+    Icon:<AddIcon />
+  }
+  isOpened = 0
     render() {
     return (
       <div data-aos="fade-down" className={style.TextCardDisplayer}>
           <div data-aos="fade-up" className={style.Gtitle}>
               <p data-aos="fade-up" className={style.Title}>{this.props.data.title}</p>
-              <p data-aos="fade-down" className={style.Icon}><AddIcon/></p>
+              <p data-aos="fade-down" onClick={()=>{
+                const status = this.isOpened%2==1?"none":"block"
+                const Icon = this.isOpened%2==1?<AddIcon />:<RemoveIcon />
+                
+                document.getElementById("ZoneNum"+this.props.id).style.display=status
+                this.setState({
+                  Icon:Icon
+                })
+                this.isOpened++
+              }} className={style.Icon}>{this.state.Icon}</p>
             </div>
-          <div data-aos="fade-down" className={style.Content}>{this.props.data.text}</div>
+          <div data-aos="fade-down" id={"ZoneNum"+this.props.id} className={style.Content}>{this.props.data.text}</div>
       </div>
     );
   }
